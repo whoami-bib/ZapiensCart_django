f6fb09e74cf5cb02dad18044dd53c8626c35d17f
BIBIN <bibin6724@gmail.com>
2022-10-07 16:30:46 +0530

    Add address updated successfully

--- store/views.py
+++ store/views.py
@@ -22,8 +22,24 @@ def store(request, category_slug=None):
         categories = get_object_or_404(Category, slug=category_slug)
         products = Product.objects.filter(category=categories, is_available=True)
         for single_product in products:
-            product_offer = single_product.price-((single_product.category.offer*single_product.price)/100 )
-        paginator = Paginator(products, 8)
+            product1=[]
+        for single_product in products:
+            
+            product1.append({
+                'id': single_product.id,
+                'product_name': single_product.product_name,  
+                'slug': single_product.slug,         
+                'descrbtion' :single_product.descrbtion,    
+                'price'  : single_product.price,       
+                'image'  :single_product.image,       
+                'stock'  : single_product.stock,       
+                'is_available' : single_product.is_available, 
+                'category'   :single_product.category,   
+                'created_date' :single_product.created_date, 
+                'modified_date':single_product.modified_date,
+                'offer': (single_product.price-(single_product.category.offer * single_product.price)/100),
+                 })
+        paginator = Paginator(product1, 8)
         page = request.GET.get('page')
         paged_products = paginator.get_page(page)
         product_count = products.count()
@@ -86,8 +102,25 @@ def search(request):
         if keyword:
             products=Product.objects.order_by("-created_date").filter(Q(descrbtion__icontains=keyword) | Q(product_name__icontains=keyword))
             product_count=products.count()
+            product1=[]
+        for single_product in products:
+            
+            product1.append({
+                'id': single_product.id,
+                'product_name': single_product.product_name,  
+                'slug': single_product.slug,         
+                'descrbtion' :single_product.descrbtion,    
+                'price'  : single_product.price,       
+                'image'  :single_product.image,       
+                'stock'  : single_product.stock,       
+                'is_available' : single_product.is_available, 
+                'category'   :single_product.category,   
+                'created_date' :single_product.created_date, 
+                'modified_date':single_product.modified_date,
+                'offer': (single_product.price-(single_product.category.offer * single_product.price)/100),
+                 })
             context={
-                'products':products,
+                'products':product1,
                 'product_count' : product_count,
             }
 
